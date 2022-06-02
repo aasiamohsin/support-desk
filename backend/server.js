efee1906ac73b445f7bd0a27e52e2a7b44b5934e
Aasia Mohsiin <aasiamohsin.91@gmail.com>
2022-06-02 16:55:16 +0500

    Basic Express Server Setup

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,19 @@
-console.log('server...');
+// Import express module to ctreate express app
+const express = require('express');
+
+// Initialize App
+const app = express();
+
+// Loads env file contents into process.env
+const dotenv = require('dotenv').config();
+
+// Initializing port
+const PORT = process.env.PORT || 8000;
+
+// Returns response
+app.get('/', (req, res) =>
+  res.status(200).json({ message: 'Welcome to Support Desk!' })
+);
+
+// Listen for port/connection and returns server
+app.listen(PORT, () => console.log(`Server Started on port  ${PORT}`));
