7c9ba199cd18147ae8da135f13c9d12b69a19779
Aasia Mohsiin <aasiamohsin.91@gmail.com>
2022-06-04 04:20:22 +0500

    Added Schema
    Created userModel & registerUser

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,3 +1,5 @@
+const bcrypt = require('bcryptjs/dist/bcrypt');
+const User = require('../models/userModels');
 const asyncHandler = require('express-async-handler');
 
 // @desc Register new user
@@ -8,11 +10,40 @@ const registerUser = asyncHandler(async (req, res) => {
 
   if (!name || !email || !password) {
     // return res.status(400).json({ message: 'Please include all fields.' });
-
     res.status(400);
     throw new Error('Please include all fields.');
   }
-  res.send('Register User.');
+
+  // Find if user already exists
+  const userExists = await User.findOne({ email });
+
+  // If user exists
+  if (userExists) {
+    res.status(400);
+    throw new Error('User already exists.');
+  }
+
+  // Hash the password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  // Create new user
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error('Invalid user credentials.');
+  }
 });
 
 // @desc Login user
